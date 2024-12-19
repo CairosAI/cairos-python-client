498364c7b83927e403b79d6e9fd1ed5118e4014a
Mois Moshev <mois.moshev@bottleshipvfx.com>
2024-12-19 16:25:21 +0200

    Add more workflows, tests, tox file

--- cairos_python_client/__init__.py
+++ cairos_python_client/__init__.py
@@ -1,10 +1,21 @@
+from typing import Sequence
 from cairos_types.core import Motions
 import json
 from cairos_python_lowlevel.cairos_python_lowlevel import AuthenticatedClient, Client
-from cairos_python_lowlevel.cairos_python_lowlevel.api.default import login_auth_login_post, process_message_thread_thread_id_post, process_message_nosequence_thread_thread_id_nosequence_post, get_threads_thread_get, post_thread_thread_post
+from cairos_python_lowlevel.cairos_python_lowlevel.api.default import (
+    get_avatar_with_file_avatar_uuid_get,
+    get_avatars_avatar_get,
+    login_auth_login_post,
+    process_message_thread_thread_id_post,
+    process_message_nosequence_thread_thread_id_nosequence_post,
+    get_threads_thread_get,
+    post_thread_thread_post,
+    get_thread_thread_thread_id_get)
+
 from cairos_python_lowlevel.cairos_python_lowlevel.models import BodyLoginAuthLoginPost
 from cairos_python_lowlevel.cairos_python_lowlevel.models.chat_output import ChatOutput
 from cairos_python_lowlevel.cairos_python_lowlevel.models.chat_input import ChatInput
+from cairos_python_lowlevel.cairos_python_lowlevel.models.http_validation_error import HTTPValidationError
 from cairos_python_lowlevel.cairos_python_lowlevel.models.human_message import HumanMessage
 from cairos_python_lowlevel.cairos_python_lowlevel.models.avatar_metadata import AvatarMetadata
 from cairos_python_lowlevel.cairos_python_lowlevel.models.chat_thread_public import ChatThreadPublic
@@ -37,14 +48,15 @@ def login(url: str, user: str, password: str) -> AuthenticatedClient:
 
     print(f"response: {response}")
     cookies = parse_cookies(response.headers.get("Set-Cookie"))
-
     return AuthenticatedClient(
         base_url=url,
-        token=cookies.get("id"),
+        token=cookies.get("id", ""),
         verify_ssl=False,
         cookies=cookies)
 
 def send_chat(prompt: str, thread_id: str, client: AuthenticatedClient) -> ChatOutput:
+    """ Send a prompt to AI, receive structured output, containing animations, etc.
+    """
     response = process_message_thread_thread_id_post.sync(
             thread_id=thread_id,
             client=client,
@@ -59,6 +71,9 @@ def send_chat(prompt: str, thread_id: str, client: AuthenticatedClient) -> ChatO
         raise Exception(str(response))
 
 def request_motions_sequence(prompt: str, thread_id: str, client: AuthenticatedClient) -> ChatOutput:
+    """ Similar to send_chat, but does not trigger a sequencing job.
+    Useful for when you only want to retrieve the list of motions.
+    """
     response = process_message_nosequence_thread_thread_id_nosequence_post.sync(
         thread_id=thread_id,
         client=client,
@@ -79,3 +94,22 @@ def create_thread(client: AuthenticatedClient) -> ChatThreadPublic:
     thread = (post_thread_thread_post.sync(client=client))
     assert thread is not None, "Thread should not be empty"
     return thread
+
+def get_thread_by_id(thread_id: str, client: AuthenticatedClient) -> ChatThreadPublic | HTTPValidationError | None:
+    return get_thread_thread_thread_id_get.sync(
+        thread_id=thread_id,
+        client=client)
+
+def list_avatars(client: AuthenticatedClient) -> Sequence[AvatarMetadata]:
+    avatar_response = get_avatars_avatar_get.sync(client=client)
+    if avatar_response:
+        return list(map(
+            lambda a: AvatarMetadata(**a.to_dict()),
+            avatar_response))
+    else:
+        return []
+
+def get_avatar(uuid: str, client: AuthenticatedClient) -> bytes:
+    """ Return the avatar file as bytes.
+    """
+    return get_avatar_with_file_avatar_uuid_get.sync_detailed(uuid=uuid, client=client).content
--- tests/client_test.py
+++ tests/client_test.py
@@ -1,6 +1,7 @@
 import pytest
 
 import cairos_python_client
+from cairos_python_lowlevel.cairos_python_lowlevel import AuthenticatedClient
 
 @pytest.fixture
 def user_data():
@@ -12,8 +13,30 @@ def user_data():
 def logged_in_client(user_data: tuple[str, str, str]):
     return cairos_python_client.login(*user_data)
 
-def test_list_threads(logged_in_client: cairos_python_client.AuthenticatedClient):
+def test_list_threads(logged_in_client: AuthenticatedClient):
     threads = cairos_python_client.list_threads(logged_in_client)
     print(threads)
     assert threads and len(threads) > 0
     assert threads[0].id is not None
+
+def test_create_get_thread(logged_in_client: AuthenticatedClient):
+    new_thread = cairos_python_client.create_thread(logged_in_client)
+    thread = cairos_python_client.get_thread_by_id(new_thread.id, logged_in_client)
+    print(f"Thread: {thread}")
+    assert thread, "Thread not found"
+
+def test_list_avatars(logged_in_client: AuthenticatedClient):
+    avatars = cairos_python_client.list_avatars(logged_in_client)
+    print(avatars)
+    assert avatars and len(avatars) > 0
+
+def test_motions_sequence(logged_in_client):
+    prompt = "Hugging a pig"
+    threads = cairos_python_client.list_threads(logged_in_client)
+    assert threads and len(threads) > 0
+    sequence = cairos_python_client.request_motions_sequence(
+        prompt,
+        threads[0].id,
+        logged_in_client)
+
+    assert sequence
